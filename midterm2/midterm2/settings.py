4db713400aa04c609d46e78294e26c5c4258c994
Akbar <akbarkozhakhmetov@gmail.com>
2023-04-24 10:55:31 +0600

    requirements

--- midterm2/midterm2/settings.py
+++ midterm2/midterm2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'aspectteam'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["lantanum.pythonanywhere.com"]
 
 
 # Application definition
